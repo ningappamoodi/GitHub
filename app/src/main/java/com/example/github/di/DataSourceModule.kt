e120a31154c3c0c2a1b4b320a12bd0697088269c
Ningappa Moodi <ningappa.moodi@gmail.com>
2020-12-01 02:06:47 +0530

    Refactored classes

--- app/src/main/java/com/example/github/di/DataSourceModule.kt
+++ app/src/main/java/com/example/github/di/DataSourceModule.kt
@@ -3,5 +3,5 @@ package com.example.github.di
 import com.example.github.domain.datasource.UserDataSource
 import org.koin.dsl.module
 
-val dataSourceModule = module{ single {  UserDataSource(get()) }
+val dataSourceModule = module{ single {  UserDataSource(get(), get()) }
 }
